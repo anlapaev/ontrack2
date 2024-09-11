81aa7690c928de3aec45b89797dd3a9a3c89d7ae
Андрей Лапаев <an.lapaev@proton-group.ru>
2024-09-11 14:28:48 +0300

    03. Nav template & 04. v-for directive

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,25 @@
       </div>
     </a>
   </header>
-  
+
+  <main class="flex flex-grow flex-col"></main>
+
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li v-for="page in navItems" :key="page" class="flex-1">
+        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
+          <ClockIcon v-if="page === 'timeline'" class="h-6 w-6" />
+          <ListBulletIcon v-else-if="page === 'activities'" class="h-6 w-6" />
+          <ChartBarIcon v-else class="h-6 w-6" /> {{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+
+const navItems = ['timeline', 'activities', 'progress']
 </script>
