4e288d54ae81a0c352f3a8438418e66bc3bce9cd
Андрей Лапаев <an.lapaev@proton-group.ru>
2024-09-11 17:27:16 +0300

    10. Navigate between pages

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,12 +1,12 @@
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-        <NavItem
+      <NavItem
         v-for="(icon, page) in navItems"
         :key="page"
         :href="`#${page}`"
         :class="{ 'pointer-events-none bg-gray-200': page === currentPage }"
-        @click="currentPage = page"
+        @click="emit('navigate', page)"
       >
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
@@ -15,24 +15,17 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import NavItem from './NavItem.vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constans'
+
+defineProps(['currentPage'])
+
+const emit = defineEmits(['navigate'])
 
 const navItems = {
   [PAGE_TIMELINE]: ClockIcon,
   [PAGE_ACTIVITIES]: ListBulletIcon,
   [PAGE_PROGRESS]: ChartBarIcon
-}
-
-const currentPage = ref(normalizePageHash())
-function normalizePageHash() {
-  const hash = window.location.hash.slice(1)
-  if (Object.keys(navItems).includes(hash)) {
-    return hash
-  }
-  window.location.hash = PAGE_TIMELINE
-  return PAGE_TIMELINE
 }
 </script>
