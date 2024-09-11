d03a6ca67ac779b8bade44f274e4a868247588be
Андрей Лапаев <an.lapaev@proton-group.ru>
2024-09-11 15:35:51 +0300

    06. Header and nav components

---
+++ src/components/TheHeaderProgress.vue
@@ -0,0 +1,16 @@
+<template>
+  <a href="#" class="text-sm">
+    <div v-if="true" class="flex items-center gap-1">
+      Day complete!
+      <CheckCircleIcon class="h-7 w-7 text-green-500" />
+    </div>
+    <div v-else class="flex items-center gap-1">
+      Progress: <span class="font-mono">50%</span>
+      <div class="h-3 w-3 rounded-full bg-red-500"></div>
+    </div>
+  </a>
+</template>
+
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
