9461eeae7cadaa39e303d8cc10cf6a8dff36ed23
Andrey Lapaev <boss.lapaev01@mail.ru>
2024-09-16 17:36:15 +0300

    33. Create activity form

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -8,9 +8,10 @@
 
 <script>
 // Создали script, чтобы кнопка создавалась только один раз
-import { BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL } from '../constants'
+import { BUTTON_TYPE_PRIMARY, BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL } from '../constants'
 
 const typeClasses = {
+  [BUTTON_TYPE_PRIMARY]: 'bg-stone-500 enabled:hover:bg-stone-600 text-white',
   [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white',
   [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200'
 }
@@ -21,7 +22,7 @@ import { isButtonTypeValid } from '../validators'
 
 defineProps({
   type: {
-    default: BUTTON_TYPE_NEUTRAL,
+    default: BUTTON_TYPE_PRIMARY,
     type: String,
     validator: isButtonTypeValid
   }
