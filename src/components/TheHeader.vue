1baf86c956a45078f7c1f2981bb24b0b17a6b6d6
Andrey Lapaev <boss.lapaev01@mail.ru>
2024-09-16 10:19:32 +0300

    test

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,9 +1,7 @@
 <template>
-  <header
-    class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-4"
-  >
-  <TheLogo />
-  <TheHeaderProgress />
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-4">
+    <TheLogo />
+    <TheHeaderProgress />
   </header>
 </template>
 
