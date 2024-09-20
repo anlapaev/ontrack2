24ad47133842e80308f8c3edb7e21d79e46b415a
Andrey Lapaev <boss.lapaev01@mail.ru>
2024-09-20 09:03:34 +0300

    38. Empty activities state

--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <ul class="divide-y">
+  <div class="flex grow flex-col">
+    <ul v-if="activities.length" class="divide-y">
       <ActivityItem
         v-for="activity in activities"
         :key="activity"
@@ -8,6 +8,7 @@
         @delete="emit('deleteActivity', activity)"
       />
     </ul>
+    <TheActivitiesEmptyState v-else/>
     <TheActivityForm @submit="emit('createActivity', $event)" />
   </div>
 </template>
@@ -16,6 +17,7 @@
 import { validateActivities, isActivityValid } from '../validators'
 import ActivityItem from '../components/ActivityItem.vue'
 import TheActivityForm from '../components/TheActivityForm.vue'
+import TheActivitiesEmptyState from '@/components/TheActivitiesEmptyState.vue';
 
 defineProps({
   activities: {
